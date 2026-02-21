fbd93d4849c2116c7de652033b983c7bd801a345
DESU.yo? <aquelecaralatl29@gmail.com>
2026-02-21 14:19:30 -0300

    Add files via upload

---
+++ Practicing/roll_dice/dice.py
@@ -0,0 +1,33 @@
+from random import randint as rint
+
+print('\n!! Use apenas números\n')
+
+try:
+    dice = int(input('Qual dado deseja rolar?\n>>> d'))
+except ValueError:
+    print('Valor inserido é inválido.')
+
+multirll = str(input('Deseja rolar mais de um dado?\n0. Não\n1. Sim\n>>> '))
+if multirll == '1':
+    try:
+        un = int(input('Rolará quantos?\n>>> '))
+    except ValueError:
+        print('Valor inserido é inválido.')
+    listrolls = []
+    print('\nRolou {}d{}\nCaiu:\n'.format(un, dice))
+    for rll in range(un):
+        roll = rint(1, dice)
+        if roll == dice:
+            print(roll, 'CRITICAL!')
+        else:
+            print(roll)
+        listrolls.append(roll)
+    result = sum(listrolls)
+    print('Total: ', result)
+elif multirll == '0':
+    roll = rint(1, dice)
+    if roll == dice:
+        print('CRITICAL')
+    print('\nRodou d{}!\nCaiu {}'.format(dice, roll))
+else:
+    print('ué')
